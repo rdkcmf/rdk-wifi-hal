76b52b3e191e5f5b98ed8bd00c737ee6cb28b928
Viviane Cordeiro <viviane.cordeirodasilva@sky.uk>
2022-03-23 09:26:56 +0000

    RDKB-41123: Compliance Issue in rdk-wifi-hal
    Reason for change: new files are missing headers and credits
    for reused code are missing
    Test Procedure: None
    Risks: Low
    
    Signed-off-by: Viviane Cordeiro <viviane.cordeirodasilva@sky.uk>
    (cherry picked from commit c92bcc8e4941d0b57d55666478fc77527b3980fb)
    Change-Id: I7a5ff15d089ded244afd2b9a0dac0e7cebeca482

--- src/wifi_hal_priv.h
+++ src/wifi_hal_priv.h
@@ -1,3 +1,21 @@
+/*
+ * If not stated otherwise in this file or this component's Licenses.txt file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2018 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 #ifndef WIFI_HAL_PRIV_H
 #define WIFI_HAL_PRIV_H
 
@@ -60,6 +78,10 @@ extern "C" {
 
 #define WIFI_HAL_MAJOR  3
 #define WIFI_HAL_MINOR  0
+/*
+ * Copyright (c) 2003-2013, Jouni Malinen <j@w1.fi>
+ * Licensed under the BSD-3 License
+*/
 #if 0
 #define RSN_SELECTOR(a, b, c, d) \
    ((((unsigned int) (a)) << 24) | (((unsigned int) (b)) << 16) | (((unsigned int) (c)) << 8) | \
